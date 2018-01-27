aa27b707fcfe780ff82ae76c7c51b841718b2650
WildPorpoiseGaming <wildporpoisegaming1@gmail.com>
2018-01-27 03:43:36 -0600

    finishes placing the text in the parallax component

--- src/components/Story.jsx
+++ src/components/Story.jsx
@@ -6,7 +6,8 @@ class Story extends React.Component {
   render() {
     return (
         <div className="Story">
-          <h1>Story Component</h1>
+          <h1>Our Story</h1>
+          <p>Last year, U.S. companies struggle to fill more than 1.3 million job openings in software development. The market is responding to this demand, but most students turn to standalone code development boot camps for the answer.</p>
         </div>
       )
   }
